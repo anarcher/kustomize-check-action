81dd7b22bddeb4f3aac6b4d0a2998d31ea36f3f1
anarcher <anarcher@gmail.com>
2020-03-27 02:52:46 +0900

    feat(config): add config

---
+++ pkg/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v6"
+)
+
+type GitHub struct {
+	Workflow   string `env:"GITHUB_WORKFLOW"`
+	Action     string `env:"GITHUB_ACTION"`
+	Actor      string `env:"GITHUB_ACTOR"`
+	Repository string `env:"GITHUB_REPOSITORY"`
+	Commit     string `env:"GITHUB_SHA"`
+	EventName  string `env:"GITHUB_EVENT_NAME"`
+	EventPath  string `env:"GITHUB_EVENT_PATH"`
+	Ref        string `env:"GITHUB_REF"`
+}
+
+type Inputs struct {
+	Paths []string `env:"INPUT_PATHS" envSeparator:"\n"`
+}
+
+type Config struct {
+	GitHub GitHub
+	Inputs Inputs
+}
+
+func LoadConfigFromEnv() (*Config, error) {
+	github := GitHub{}
+	if err := env.Parse(&github); err != nil {
+		return nil, err
+	}
+
+	inputs := Inputs{}
+	if err := env.Parse(&inputs); err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		GitHub: github,
+		Inputs: inputs,
+	}, nil
+}

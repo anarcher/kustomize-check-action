0813def2f2ff4571e8dca4b2c45bc8d470025043
anarcher <anarcher@gmail.com>
2020-03-30 01:54:58 +0900

    feat(config): add GithubEvent for reading num of commits

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,11 @@
 package config
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
 	"github.com/caarlos0/env/v6"
 )
 
@@ -15,14 +20,57 @@ type GitHub struct {
 	Ref        string `env:"GITHUB_REF"`
 }
 
+type GithubEvent struct {
+	PullRequest struct {
+		Commits int `json:"commits"`
+	} `json:"pull_request,omitempty"`
+	Commits []interface{} `json:"commits,omitempty"`
+}
+
+func (e GithubEvent) TotalCommits() int {
+	return len(e.Commits) + e.PullRequest.Commits
+}
+
 type Inputs struct {
-	Paths        []string `env:"INPUT_PATHS" envSeparator:"\n"`
-	CommitBefore string   `env:"INPUT_GITHUB_SHA_BEFORE"`
+	Paths []string `env:"INPUT_PATHS" envSeparator:"\n"`
 }
 
 type Config struct {
-	GitHub GitHub
-	Inputs Inputs
+	GitHub      GitHub
+	GithubEvent GithubEvent
+	Inputs      Inputs
+}
+
+func (c Config) Print() {
+	fmt.Printf("GITHUB_SHA:%s\n", c.GitHub.Commit)
+	fmt.Printf("INPUT_PATHS:%s\n", c.Inputs.Paths)
+	fmt.Printf("TotalCommits: %v\n", c.GithubEvent.TotalCommits())
+}
+
+func (i *Inputs) trimPaths() {
+	var paths []string
+	for _, p := range i.Paths {
+		path := strings.TrimSpace(p)
+		if path != "" {
+			paths = append(paths, path)
+		}
+	}
+	i.Paths = paths
+}
+
+func loadGitHubEvent(path string) (GithubEvent, error) {
+	e := GithubEvent{}
+
+	bs, err := ioutil.ReadFile(path)
+	if err != nil {
+		return e, err
+	}
+
+	if err := json.Unmarshal(bs, &e); err != nil {
+		return e, err
+	}
+
+	return e, nil
 }
 
 func LoadConfigFromEnv() (*Config, error) {
@@ -35,9 +83,16 @@ func LoadConfigFromEnv() (*Config, error) {
 	if err := env.Parse(&inputs); err != nil {
 		return nil, err
 	}
+	inputs.trimPaths()
+
+	githubEvent, err := loadGitHubEvent(github.EventPath)
+	if err != nil {
+		return nil, err
+	}
 
 	return &Config{
-		GitHub: github,
-		Inputs: inputs,
+		GitHub:      github,
+		GithubEvent: githubEvent,
+		Inputs:      inputs,
 	}, nil
 }
